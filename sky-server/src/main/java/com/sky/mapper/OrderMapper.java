9a1714916bd8778323477049bff785094ee0d7bf
master <1686407441@qq.com>
2025-04-08 21:03:12 +0800

    营业额数据统计业务开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -60,4 +61,11 @@ public interface OrderMapper {
      */
     @Select("select * from orders where status = #{status} and order_time < #{time}")
     List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime time);
+
+    /**
+     * 动态统计营业额
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map map);
 }
